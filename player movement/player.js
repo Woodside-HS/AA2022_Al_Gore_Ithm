669855e564ef4001cb8f23119402e16081c49c2b
805933 <805933@seq.org>
2022-01-10 20:34:56 -0800

    wall collision
    walls made out of lines and are mostly working just slight issues on the corners

--- player movement/player.js
+++ player movement/player.js
@@ -1,24 +1,36 @@
 class Player{
-  constructor(x, y, color){
+  constructor(x, y, color, size){
+    this.size = size;
     this.pos = new JSVector(x,y);
     this.vel = new JSVector(0,0);
+    this.posCalc = new JSVector(0,0);
+    this.controlUp = false;
+    this.controlLeft = false;
+    this.controlDown = false;
+    this.controlRight = false;
     this.color = color;
+    this.wallScore = 0;
     this.canvas = document.getElementById("cnv");
     this.ctx = this.canvas.getContext("2d");
   }
   run(){
     this.update();
     this.draw();
+    //this.wall.getPlayerCoords(this.pos.x, this.pos.y, this.size);
   }
   update(){
     this.pos.add(this.vel)
+    this.posCalc.x = this.pos.x + this.size/2; //allows all collision to be based on the center rather than the conrer
+    this.posCalc.y = this.pos.y + this.size/2; //reduces redundancy down the line
   }
   draw(){
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.pos.x, this.pos.y, 50, 50);
-    ctx.fillStyle = "black";
-    ctx.fillRect(this.pos.x+30,this.pos.y+20, 10, 10);
-    ctx.stroke();
+    this.ctx.font = '24px serif';
+    this.ctx.fillText("PlayerX" + this.posCalc.x + "PlayerY" + this.posCalc.y, 30, 30); //debug testing
+    this.ctx.beginPath();
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(this.pos.x, this.pos.y, this.size, this.size);
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(this.pos.x+this.size*3/5,this.pos.y+this.size*2/5, this.size/5, this.size/5);
+    this.ctx.stroke();
   }
 }

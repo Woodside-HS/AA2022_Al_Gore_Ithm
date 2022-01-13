13792938e432e82db3b22fd6d4196d3ea96a06bc
Shai <803951@seq.org>
2022-01-13 08:33:18 -0800

    test player

--- LevelGeneration/main.js
+++ LevelGeneration/main.js
@@ -2,13 +2,29 @@ window.addEventListener("load", init);
 
 var cnv,ctx;
 var mazeGenerator;
+var keys;
+var player;
+
+window.addEventListener('keypress',keyDown);
+window.addEventListener('keyup',keyUp);
+
+function keyDown(e) { //testing next level function
+  keys[e.code] = true;
+}
+function keyUp(e){
+  keys[e.code] = false;
+}
 
 function init(){
     cnv = document.getElementById("cnv");
     ctx = cnv.getContext("2d");
 
-    let cellSize = 50;
+    keys = [];
+
+    let cellSize = 100;
     mazeGenerator = new MazeGenerator(new JSVector(0,0),cellSize,cnv.height/cellSize,cnv.width/cellSize,ctx);
+
+    player = new Player(cellSize/2,cellSize/2,cellSize/10,new Color(255,0,0,1),5,ctx);
 
     animate();
 }
@@ -23,4 +39,24 @@ function animate() {
 
 function update(){
   mazeGenerator.update();
+  processInput();
+  player.update();
+}
+
+function processInput(){
+  let dx = 0;
+  let dy = 0;
+  if(keys["KeyW"]){
+    dy = -1;
+  }
+  else if(keys["KeyS"]){
+    dy = 1;
+  }
+  if(keys["KeyD"]){
+    dx = 1;
+  }
+  else if(keys["KeyA"]){
+    dx = -1;
+  }
+  player.changeTarget(dx,dy);
 }
---
+++ LevelGeneration/player.js
@@ -0,0 +1,32 @@
+function Player(x,y,rad,color,speed,ctx){
+  this.pos = new JSVector(x,y);
+  this.targetPos = new JSVector(x,y);
+  this.rad = rad;
+  this.color = color;
+  this.speed = speed;
+  this.ctx = ctx;
+}
+
+Player.prototype.update = function(){
+  this.move(0.2);
+  this.draw();
+}
+
+Player.prototype.move = function(t){
+  let dir = JSVector.subGetNew(this.targetPos,this.pos);
+  dir.setMagnitude(dir.getMagnitude()*t);
+  this.pos.add(dir);
+}
+
+Player.prototype.draw = function(){
+  this.ctx.beginPath();
+  this.ctx.fillStyle = this.color.toString();
+  this.ctx.arc(this.pos.x,this.pos.y,this.rad,0,Math.PI*2);
+  this.ctx.fill();
+}
+
+Player.prototype.changeTarget = function(dx,dy){
+  let delta = new JSVector(dx,dy);
+  if(delta.getMagnitude()>0) delta.setMagnitude(this.speed);
+  this.targetPos.add(delta);
+}

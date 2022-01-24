4f0c629a0db44a04fbc7e3e9913aeff98cc14151
801742 <801742@seq.org>
2022-01-24 12:21:57 -0800

    Drag feature finished

--- ParticleSystem/main.js
+++ ParticleSystem/main.js
@@ -1,31 +1,17 @@
 window.addEventListener("load", init);
 
-let particleSystems = [];
-var mouseStatus;
-var mousePos, cnv, ctx;
+var mousePos,particleSystem, mouseStatus, cnv, ctx;
 
 function init(){
-
     cnv = document.getElementById("cnv");
     ctx = cnv.getContext("2d");
-
-    mousePos = new JSVector(0,0);
-    let pos = new JSVector(cnv.width/2,cnv.height/2);
-    let acc = JSVector.subGetNew(new JSVector(0,0),pos);
-    acc.normalize();
-    acc.setMagnitude(.8);
-
-    animate()
+    animate();
 }
+
 function update(){
   if(mouseStatus){
-    for(let i = 0; i < particleSystems.length; i++){
-      particleSystems[i].run();
+      particleSystem.run();
     }
-    mousePos = new JSVector(MouseEvent.clientX, MouseEvent.clientY);
-    //particleSystems.push(new ParticleSystem(cnv.width/2,cnv.height/2, .8, mousePos));
-  }
-
 }
 function animate(){
     ctx.clearRect(0,0,cnv.width,cnv.height);
--- ParticleSystem/particleSystem.js
+++ ParticleSystem/particleSystem.js
@@ -1,26 +1,25 @@
+//Listener to determine when click to make particle system
 window.addEventListener("mousedown", ()=>{
   mouseStatus = true;
-  mousePos = new JSVector(event.offsetX, event.offsetY);
-  particleSystems.push(new ParticleSystem(cnv.width/2,cnv.height/2, .8, mousePos));
+  particleSystem = new ParticleSystem(cnv.width/2,cnv.height/2);
 });
-
+//Listener to determine when to stop running particle System
 window.addEventListener("mouseup", ()=>{
   mouseStatus = false;
-  particleSystems.splice(0, 1);
 });
+//Listner to track mouse movement to be able to drag and shoot.
+window.addEventListener("mousemove", (event) => {
+  mousePos = new JSVector(event.clientX, event.clientY);
+})
 
-function ParticleSystem(x,y,accMag,destination){
+function ParticleSystem(x,y){
   this.pos = new JSVector(x,y);
-  this.acc = destination;
-  this.acc.sub(this.pos);
-  this.acc.setMagnitude(.9);
   this.particles = [];
-
 }
 
 ParticleSystem.prototype.run = function(){
-  this.render();
   this.update();
+  this.render();
 }
 
 ParticleSystem.prototype.render = function(){
@@ -28,6 +27,10 @@ ParticleSystem.prototype.render = function(){
 }
 
 ParticleSystem.prototype.update = function(){
+  this.setDir(mousePos);
+
+
+  //PARTICLE LIFESPAN
   for(var i = this.particles.length-1;i>=0;i--){
     let p = this.particles[i];
     p.run();
@@ -36,7 +39,7 @@ ParticleSystem.prototype.update = function(){
     }
   }
 }
-
+//setDir gets new vector from the click and orgin of particle system to shoot in correct dir
 ParticleSystem.prototype.setDir = function(enemypos){
   this.acc = JSVector.subGetNew(enemypos,this.pos).setMagnitude(.8);
 }

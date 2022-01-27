a0607c41b19b138eb6e5217bccce84dccfdf5fea
Shai Dickman <shai.j.dickman@gmail.com>
2022-01-26 19:16:35 -0800

    functional player

--- Player/main.js
+++ Player/main.js
@@ -1,14 +1,41 @@
 window.addEventListener("load", init);
 
 var cnv,ctx;
+var mousePos,mouseStatus;
+var keys;
 var player;
+
+window.addEventListener('keypress',keyDown);
+window.addEventListener('keyup',keyUp);
+window.addEventListener("mousedown",mouseDown);
+window.addEventListener("mouseup",mouseUp);
+window.addEventListener("mousemove",mouseMove);
+
+function mouseDown(){
+  mouseStatus = true;
+}
+function mouseUp(){
+  mouseStatus = false;
+}
+function mouseMove(){
+  mousePos = new JSVector(event.clientX, event.clientY);
+}
+function keyDown(e) { //testing next level function
+  keys[e.code] = true;
+}
+function keyUp(e){
+  keys[e.code] = false;
+}
 
 function init(){
   cnv = document.getElementById("cnv");
   ctx = cnv.getContext("2d");
+  keys = [];
+  mousePos = new JSVector(0,0);
+  mouseStatus = false;
 
-  player = new Player();
-  
+  player = new Player(cnv.width/2,cnv.height/2,15,new Color(255,0,0,1),5,100,ctx);
+
   animate();
 }
 
@@ -21,5 +48,5 @@ function animate(){
 }
 
 function update(){
-
+  player.run();
 }
--- Player/player.js
+++ Player/player.js
@@ -1,3 +1,41 @@
-function Player(){
+function Player(x,y,rad,clr,speed,life,ctx){
 
+  Character.call(this,x,y,rad,clr,speed,life,ctx);
+
+  this.particleSystem = new ParticleSystem(x,y);
+  this.particleSystem.pos = this.pos;
+}
+
+Player.prototype = new Character();
+
+Player.prototype.run = function(){
+  this.processInput();
+  this.update();
+  this.shootParticles();
+}
+
+Player.prototype.processInput = function(){
+  let dx,dy = 0;
+
+  if(keys["KeyW"]){
+    dy = -1;
+  }
+  else if(keys["KeyS"]){
+    dy = 1;
+  }
+  if(keys["KeyD"]){
+    dx = 1;
+  }
+  else if(keys["KeyA"]){
+    dx = -1;
+  }
+
+  this.vel = new JSVector(dx,dy);
+}
+
+Player.prototype.shootParticles = function(){
+  if(mouseStatus){
+    this.particleSystem.generateParticles(mousePos,this.vel);
+  }
+  this.particleSystem.update();
 }

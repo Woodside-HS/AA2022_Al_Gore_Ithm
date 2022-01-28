9d6b5a593fd5b7e22db3c66191a51d4a202cf7a5
Shai <803951@seq.org>
2022-01-28 11:31:02 -0800

    reworked level generation

--- LevelGeneration/maze.js
+++ LevelGeneration/maze.js
@@ -1,4 +1,4 @@
-function MazeGenerator(cellSize,rows,cols,ctx,wallClr){
+function Maze(cellSize,rows,cols,ctx,wallClr){
   this.ctx = ctx;
   this.rows = rows;
   this.cols = cols;
@@ -15,7 +15,7 @@ function MazeGenerator(cellSize,rows,cols,ctx,wallClr){
   }
 }
 
-MazeGenerator.prototype.generateMaze = function(cell){
+Maze.prototype.generateMaze = function(cell){
   this.cells[cell].visited = true;
 
   let n = cell-this.rows;
@@ -45,30 +45,31 @@ MazeGenerator.prototype.generateMaze = function(cell){
     neighborArr[randIndex] = neighborArr[neighborArr.length-i-1];
     neighborArr[neighborArr.length-i-1] = temp;
   }
-  if(neighbor<0){
+  if(neighbor<0){ //checks if there is no available neighbor
     if(this.cells[cell].connectedTo>=0){
       this.generateMaze(this.cells[cell].connectedTo);
     }
     return;
   }
-  if(neighbor == n){
+  if(neighbor == n){ //removes northern wall
     this.cells[neighbor].wallStatus.s = false;
   }
-  if(neighbor == s){
+  if(neighbor == s){ //removes southern wall
     this.cells[cell].wallStatus.s = false;
   }
-  if(neighbor == w){
+  if(neighbor == w){ //removes western wall
     this.cells[neighbor].wallStatus.e = false;
   }
-  if(neighbor == e){
+  if(neighbor == e){ //removes eastern wall
     this.cells[cell].wallStatus.e = false;
   }
-  this.cells[neighbor].connectedTo = cell;
+  this.cells[neighbor].connectedTo = cell; //links next cell to the current cell
 
-  this.generateMaze(neighbor);
+  this.generateMaze(neighbor); //recursively calls maze generation in the next cell
 }
 
-MazeGenerator.prototype.resetGrid = function(){
+Maze.prototype.resetGrid = function(){
+  //makes 1d array for grid cells
   this.cells = [];
   for(var j = 0;j<this.rows;j++){
     for(var i = 0;i<this.cols;i++){
@@ -82,13 +83,15 @@ MazeGenerator.prototype.resetGrid = function(){
   let width = this.cols*this.cellSize;
   let height = this.rows*this.cellSize;
 
+  //gives world a border
+
   let n = new Wall(this.ctx,0,0,0,width,this.wallClr);
   let w = new Wall(this.ctx,0,0,90,height,this.wallClr);
 
   this.borderWalls = [n,w];
 }
 
-MazeGenerator.prototype.update = function(){
+Maze.prototype.update = function(){
   for(var i = 0;i<this.cells.length;i++){
     this.cells[i].draw();
   }
@@ -97,8 +100,8 @@ MazeGenerator.prototype.update = function(){
   }
 }
 
-MazeGenerator.prototype.detectCharacterCollision = function(dx,dy,character,prevMove){
-  let shiftedPos = new JSVector(character.pos.x-this.cellSize/2,character.pos.y-this.cellSize/2)
+Maze.prototype.detectCharacterCollision = function(dx,dy,character,prevMove){
+  let shiftedPos = new JSVector(character.pos.x-this.cellSize/2,character.pos.y-this.cellSize/2); //position of character shifted to center of cell
 
   let closeCells = [];
 
@@ -107,27 +110,64 @@ MazeGenerator.prototype.detectCharacterCollision = function(dx,dy,character,prev
   let i_center = Math.round(c + r*this.cols);
   let i_n = i_center-this.cols;
   let i_w = i_center-1;
+  let i_s = i_center+this.cols;
+  let i_e = i_center+1;
+  let i_nw = i_n-1;
+  let i_ne = i_n+1;
+  let i_sw = i_s-1;
+  let i_se = i_s+1;
 
+  //loads all surrounding cells *******************//
   let center = this.cells[i_center]; //current cell
   let n = this.cells[i_n]; //top cell
   let w = this.cells[i_w]; //left cell
+  let s = this.cells[i_s];
+  let e = this.cells[i_e];
+  let nw = this.cells[i_nw];
+  let ne = this.cells[i_ne];
+  let sw = this.cells[i_sw];
+  let se = this.cells[i_se];
 
-  if(center!=undefined)closeCells.push(center);
-  if(n!=undefined)closeCells.push(n);
-  if(i_center%this.cols!=0&&w!=undefined)closeCells.push(w);
+  let northBorder = i_n<0;
+  let southBorder = i_s>=this.cells.length;
+  let eastBorder = i_e%this.cols==0;
+  let westBorder = i_center%this.cols==0;
+
+  if(center!=undefined){
+    closeCells.push(center);
+  }
+
+  if(!northBorder){
+    closeCells.push(n);
+
+    if(!westBorder) closeCells.push(nw);
+    if(!eastBorder) closeCells.push(ne);
+  }
+
+  if(!southBorder){
+    closeCells.push(s);
+
+    if(!westBorder) closeCells.push(sw);
+    if(!eastBorder) closeCells.push(se);
+  }
+
+  if(!westBorder) closeCells.push(w);
+  if(!eastBorder) closeCells.push(e);
+
+  //*************************************************//
 
   let walls = [];
-
+  //loads all walls surrounding the current cell
   for(var i = 0;i<closeCells.length;i++){
     for(var k = 0;k<closeCells[i].walls.length;k++){
       walls.push(closeCells[i].walls[k]);
     }
   }
-
+  //loads broder walls of world
   for(var i = 0;i<this.borderWalls.length;i++){
     walls.push(this.borderWalls[i]);
   }
-
+  //checks if character is colliding any walls and updates velocity depending on angle of walls
   for(var i = 0;i<walls.length;i++){
     if(walls[i].isColliding(character.pos,character.rad)){
 
@@ -138,5 +178,5 @@ MazeGenerator.prototype.detectCharacterCollision = function(dx,dy,character,prev
     }
   }
 
-  return new JSVector(dx,dy);
+  return new JSVector(dx,dy); //return chenged velocity depending on wall collisions
 }

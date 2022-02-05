af273f29f6a4000c6974b6bcfb0bd743db85e15c
803951 <89233685+803951@users.noreply.github.com>
2022-02-04 19:40:52 -0800

    minor changes

--- Game/player.js
+++ Game/player.js
@@ -10,6 +10,7 @@ function Player(x,y,rad,clr,speed,life,cnv,ctx){
 Player.prototype = new Character(); //inherits character class
 
 Player.prototype.run = function(maze){
+  this.particleSystem.update(maze); //updates all particles regardless if mouse down //shoots particles if mouse down - aimed towards mouse click
   this.update(maze); //runs character update method
   this.particleSystem.pos = new JSVector(this.pos.x,this.pos.y);
   if(mouseStatus){ //only shoots when mouse down
@@ -17,5 +18,4 @@ Player.prototype.run = function(maze){
     target.sub(new JSVector(this.cnv.width/2,this.cnv.height/2));
     this.particleSystem.generateParticles(target,this.vel);
   }
-  this.particleSystem.update(maze); //updates all particles regardless if mouse down //shoots particles if mouse down - aimed towards mouse click
 }

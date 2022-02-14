646408dbcf7ee4fd67daef65ecce079b1abaa5a8
Connor Williams <88904593+805933@users.noreply.github.com>
2022-02-14 11:13:45 -0800

    bugfix

--- Item/pickup.js
+++ Item/pickup.js
@@ -10,6 +10,7 @@ class Pickup{
     this.clock = 0; //movement is based on a cycle, this keeps track of time
     this.rad = rad;
     this.img = new Image(); //kinda dumb dont know why you do this but just followed the YT vid
+    this.img.src = "IMG_4373.jpg"; 
   }
   run(){
     this.update();
@@ -25,7 +26,7 @@ class Pickup{
     this.ctx.beginPath();
     this.ctx.arc(this.pos.x, this.pos.y, this.rad,0,Math.PI*2); //draws circle
     if(this.itemType == "powerup"){
-        this.img.src = "IMG_4373.jpg"; //when we merge to main, i'll get this in the files system and rewrite the code. just looked at linked YT vid for saying img.src
+      //when we merge to main, i'll get this in the files system and rewrite the code. just looked at linked YT vid for saying img.src
         this.ctx.drawImage(this.img, this.pos.x-this.rad, this.pos.y-this.rad, this.rad*2, this.rad*2); //sets the image
         //items that are classified as powerups will have an image drawn, those that aren't will just be a circle.
 

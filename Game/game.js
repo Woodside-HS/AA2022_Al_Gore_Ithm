487d3ca4f727c73abdc837bb0aab88573788e3d0
Shai <803951@seq.org>
2022-02-18 13:19:28 -0800

    game pathway and generation completely functional

--- Game/game.js
+++ Game/game.js
@@ -12,29 +12,9 @@ function Game(cnv,ctx){
   let cellSize = 100;
   let zoomFactor = 2;
 
-  let dubbya = new Enemy(0,0,20,Color.generateRandomColor(255,1,1,false),2,500,this.cnv, this.ctx,"Files/dubbya.jpeg");
-  let cheney = new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg");
-  let newt = new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg");
-  //LEVEL 1 GENERATION**************************//
-
-  let enemies_1 = [ //populates enemy array with test enemies
-    new Enemy(0,0,20,Color.generateRandomColor(255,1,1,false),2,500,this.cnv, this.ctx,"Files/dubbya.jpeg"),
-    new Enemy(0,0,20,Color.generateRandomColor(255,1,1,false),2,500,this.cnv, this.ctx,"Files/dubbya.jpeg"),
-
-    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
-    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
-    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
-
-    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
-    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
-    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
-    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
-    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg")
-  ];
-  let level1 = new Level(6,6,cellSize,enemies_1,null,this.cnv,this.ctx,zoomFactor,"Files/cell_level1.jpeg") //creates new level with no enemies or boss parameters - null,null -
-  //*******************************************//
-
-  this.levels.push(level1);
+  this.generateLevel_1(cellSize,zoomFactor);
+  this.generateLevel_2(cellSize,zoomFactor);
+  this.generateLevel_3(cellSize,zoomFactor);
 
   for(var i = 0;i<this.levels.length;i++){
     this.levels[i].generateIcon(this.levels.length,i);
@@ -46,16 +26,63 @@ function Game(cnv,ctx){
   this.levels[this.currentLevel].load(); //loads the current level after setting up entire game
 }
 
+Game.prototype.generateLevel_1 = function(cellSize,zoomFactor){
+  let enemies_1 = [ //populates enemy array with test enemies
+    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
+
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg")
+  ];
+
+  let level1 = new Level(6,6,cellSize,enemies_1,null,this.cnv,this.ctx,zoomFactor,"Files/cell_level1.jpeg");
+
+  this.levels.push(level1);
+}
+Game.prototype.generateLevel_2 = function(cellSize,zoomFactor){
+  let enemies_2 = [ //populates enemy array with test enemies
+    new Enemy(0,0,20,Color.generateRandomColor(255,1,1,false),2,500,this.cnv, this.ctx,"Files/dubbya.jpeg"),
+
+    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
+    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
+
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg")
+  ];
+  let level2 = new Level(6,6,cellSize,enemies_2,null,this.cnv,this.ctx,zoomFactor,"Files/cell_level1.jpeg");
+
+  this.levels.push(level2);
+}
+Game.prototype.generateLevel_3 = function(cellSize,zoomFactor){
+  let enemies_3 = [ //populates enemy array with test enemies
+    new Enemy(0,0,20,Color.generateRandomColor(255,1,1,false),2,500,this.cnv, this.ctx,"Files/dubbya.jpeg"),
+    new Enemy(0,0,20,Color.generateRandomColor(255,1,1,false),2,500,this.cnv, this.ctx,"Files/dubbya.jpeg"),
+
+    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
+    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
+    new Enemy(0,0,15,Color.generateRandomColor(255,1,1,false),1.5,250,this.cnv, this.ctx,"Files/cheney.jpg"),
+
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg"),
+    new Enemy(0,0,10,Color.generateRandomColor(255,1,1,false),1,100,this.cnv, this.ctx,"Files/newt.jpg")
+  ];
+  let level3 = new Level(6,6,cellSize,enemies_3,null,this.cnv,this.ctx,zoomFactor,"Files/cell_level1.jpeg");
+
+  this.levels.push(level3);
+}
+
 Game.prototype.nextLevel = function(){
   if(this.currentLevel<this.levels.length-1){
     this.setLevel(this.currentLevel+1);
-    return true;
+    this.levelPath.nextLevel();
   }
-  return false;
 }
 
 Game.prototype.setLevel = function(level){
   this.currentLevel = level;
+  this.levels[this.currentLevel].load();
 }
 
 Game.prototype.update = function(){
@@ -74,7 +101,8 @@ Game.prototype.update = function(){
     case gameStates.PlayingLevel:
       this.levels[this.currentLevel].update();
       if(this.levels[this.currentLevel].checkLevelStatus()){
-        if(!this.nextLevel()) this.gameState = gameStates.WorldMap;
+        this.nextLevel();
+        this.gameState = gameStates.WorldMap;
       }
       break;
     default:

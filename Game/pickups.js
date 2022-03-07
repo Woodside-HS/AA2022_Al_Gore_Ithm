915247a99e8104fcc18310862ae68c9622c3b5a6
803951 <89233685+803951@users.noreply.github.com>
2022-03-07 13:34:14 -0800

    minor changes

--- Game/pickups.js
+++ Game/pickups.js
@@ -15,8 +15,8 @@ Pickups.prototype.run = function(){
   this.frame++;
   if(this.frame%120==0) this.time = 0;
   this.pos = new JSVector(this.basePos.x, this.basePos.y+this.scale*Math.sin(this.frame*Math.PI/60));
-  this.draw(this.pos);
+  this.draw();
 }
-Pickups.prototype.draw = function(pos){
-  if(this.img.src!=undefined&&this.img.src!="") this.ctx.drawImage(this.img,0,0,this.img.width,this.img.height,pos.x-this.rad,pos.y-this.rad,this.rad*2,this.rad*2);
+Pickups.prototype.draw = function(){
+  if(this.img.src!=undefined&&this.img.src!="") this.ctx.drawImage(this.img,0,0,this.img.width,this.img.height,this.pos.x-this.rad,this.pos.y-this.rad,this.rad*2,this.rad*2);
 }
